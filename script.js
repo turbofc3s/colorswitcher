6c096e803563fcead2c403f6c34f55490c04fd1a
jeffrey burke <burkejeffrey56@yahoo.com>
2022-04-06 20:59:34 -0400

    second commit. background does not change color

---
+++ script.js
@@ -0,0 +1,15 @@
+/* Project 2: Build a background color switcher.
+
+Requirements:
+-Create a Github repository for this project. 
+-Create an HTML select dropdown with at least 5 color options in it
+-use the "onchange" attribute so that every time a user selects a color 
+using the dropdown, it calls your JavaScript function that changes 
+the background color of the BODY to the color selected in the 
+dropdown
+*/
+
+let function colorFunction(value) {
+		document.body.style.backgroundColor = 'value';
+
+}
